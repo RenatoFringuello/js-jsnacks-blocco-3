b29651cadf88af6e0e9849e9b1e39eb9a964bf94
RenatoFringuello <renatofringuello@hotmail.it>
2022-12-01 12:14:49 +0100

    add rick corrections

--- snack-rick/js/snack2.js
+++ snack-rick/js/snack2.js
@@ -8,12 +8,10 @@
 const startingList = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 const resultList = [];
 
-let i = 0;
-while (i < 10){
+while (resultList.length < 10){
     let randomIndex = Math.floor(Math.random() * startingList.length);
     if(!resultList.includes(startingList[randomIndex])){
         resultList.push(startingList[randomIndex]);
-        i++;
     }
 } 
 console.log(resultList);
