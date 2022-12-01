290b99c32efe76a74d76b8618ea69dc49e7bb392
RenatoFringuello <renatofringuello@hotmail.it>
2022-12-01 11:36:40 +0100

    add corrections

--- snack-rick/js/snack2.js
+++ snack-rick/js/snack2.js
@@ -5,16 +5,16 @@
  * 10 numeri presi randomicamente dalla lista, diversi tra loro.
  *
  */
- const startingList = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
- const resultList = [];
- 
- for (let i = 0; i < 10; i++) {
-     let randomIndex = Math.floor(Math.random() * 10);
-     if(!resultList.includes(randomIndex)){
-         resultList.push(randomIndex);
-     }
-     else{
-         i--;
-     }
- }
- console.log(resultList);
+const startingList = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const resultList = [];
+
+for (let i = 0; i < 10; i++) {
+    let randomIndex = Math.floor(Math.random() * startingList.length);
+    if(!resultList.includes(startingList[randomIndex])){
+        resultList.push(startingList[randomIndex]);
+    }
+    else{
+        i--;
+    }
+}
+console.log(resultList);
